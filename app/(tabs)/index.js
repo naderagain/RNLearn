077259ab04bd0d118ea63fe2d2f83061427ad772
Nader Ahmed <nader.ahmed@hh2.com>
2024-08-03 16:20:24 -0400

    how to use safe areas
    import {SafeAreaView, SafeAreaProvider} from "react-native-safe-area-context"
    
       <SafeAreaProvider>
                <SafeAreaView>

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,17 +1,29 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Nadertest from '../../components/Nadertest'
+
+// you are importing 2 components: 
+// npm i 
+import {SafeAreaView, SafeAreaProvider} from "react-native-safe-area-context"
 export default function index(){
 
     return (
 
 
           <>  
+
+
+          <SafeAreaProvider>
+            <SafeAreaView>
+
+            <Text>inasdffdsdex</Text>
+          <Nadertest></Nadertest>
           <Text>index</Text>
 
-          <Nadertest></Nadertest>
-                    <Text>index</Text>
 
+            </SafeAreaView>
+          </SafeAreaProvider>
+          
           
 
           </>
