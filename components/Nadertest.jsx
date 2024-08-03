063489a3988b1a6c7a9c5aefb3f3808d836f4b79
Nader Ahmed <nader.ahmed@hh2.com>
2024-08-03 15:27:01 -0400

    formatting worked

--- components/Nadertest.jsx
+++ components/Nadertest.jsx
@@ -1,29 +1,15 @@
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { View, Text } from 'react-native';
-import React from 'react';
- // A component always start with a function and make sure method name is capital
+import { View, Text } from "react-native";
+import React from "react";
+// A component always start with a function and make sure method name is capital
 // u need to also write export because you wnt to use it outside
-export default function Nadertest(){
-
-
-
-    return (
-
-
-        <> 
-        
-        <Text>Hello i am condicu</Text>
-        <TouchableOpacity>
-      
+export default function Nadertest() {
+  return (
+    <>
+      <Text>Hello i am condicu</Text>
+      <TouchableOpacity>
         <Text>Touch me</Text>
-
-
-        </TouchableOpacity>
-        
-        
-        </>
-
-
-    );
-
+      </TouchableOpacity>
+    </>
+  );
 }
