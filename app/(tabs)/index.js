49d08515cc049e258a3f3b51e430e864426aea22
Nader Ahmed <nader.ahmed@hh2.com>
2024-08-07 18:15:21 -0400

    uneeded tags <>   </>

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -11,7 +11,7 @@ export default function index(){
     return (
 
 
-          <>  
+         
 
 
           <SafeAreaProvider>
@@ -33,8 +33,7 @@ export default function index(){
           </SafeAreaProvider>
           
           
-
-          </>
+ 
       )
 
 
