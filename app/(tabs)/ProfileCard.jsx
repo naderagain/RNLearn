6d05a85962af75e858c8bf443f1bb730042fdc42
Nader Ahmed <nader.ahmed@hh2.com>
2024-08-11 13:08:37 -0400

    fixed formatting

--- app/(tabs)/ProfileCard.jsx
+++ app/(tabs)/ProfileCard.jsx
@@ -1,39 +1,26 @@
 import { TouchableOpacity, View, Text, Image } from "react-native";
-import {s} from "./ProfilCard.style"
-export function ProfileCard(){
-
-    return <View  style={s.container}>
-
-
+import { s } from "./ProfilCard.style";
+export function ProfileCard() {
+  return (
+    <View style={s.container}>
       <View style={s.header}>
-
+        <View>
+          <Image
+            style={s.avatar}
+            source={{ uri: "https://i.pravatar.cc/300" }}
+          />
+        </View>
+      </View>
 
       <View>
-      <Image style={s.avatar} source = { {uri: "https://i.pravatar.cc/300"}}      />
+        <Text>asfasfsdfafasfafds</Text>
+        <Text>asfasfsdfafasfafds</Text>
       </View>
-       
-      
-      </View>
- 
 
-
-<View>
-
-<Text>asfasfsdfafasfafds</Text>
-<Text>asfasfsdfafasfafds</Text>
-</View>
-
-
-
-
-
-     {/* <View style={s.social} >
+      {/* <View style={s.social} >
 
 
      </View> */}
-
-
-
-
     </View>
+  );
 }
