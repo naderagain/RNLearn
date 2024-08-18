c44fa19af3b386abc301ee2c631cbb53dcdd76b3
Nader Ahmed <nader.ahmed@hh2.com>
2024-08-18 10:58:24 -0400

    how to create a button with some text underneath it

--- app/(tabs)/AgeCounter.jsx
+++ app/(tabs)/AgeCounter.jsx
@@ -1,19 +1,28 @@
 
 import { StyleSheet, View, Text } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 
 
 export function AgeCounter(){
 
 
+    let age = 30;
+
+    function increaseAge(){
+
+        age++;
+    }
+
+
     return <>
 
 
-        
-        <View> 
-
-        <Text>askjfhaskjfh</Text>
-
-        </View>
+<TouchableOpacity style = {{
+    width: 40,
+    backgroundColor: "#0000FF" }}   >
+    <Text>Increase age</Text>
+</TouchableOpacity>
+<Text>Increase age</Text>
           
 
         </>
--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -21,7 +21,7 @@ export default function index(){
 
            </SafeAreaView>
           </SafeAreaProvider>
-          
+            
           
  
       );
